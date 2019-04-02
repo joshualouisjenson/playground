47a1a2b27ea334709564e417799c7f743fbf9679
Joshua Jenson <jjenson@live.com>
2019-04-02 16:24:02 -0500

    - Home page styles and content

--- src/app/pages/home/home/home.component.ts
+++ src/app/pages/home/home/home.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'jj-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  @HostBinding('class') classes = 'flex-fill';
 
 }
